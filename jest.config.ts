a536c223416303207ebb43568afb80a3b426f3ad
smy-101 <824860484@qq.com>
2022-07-01 09:26:18 +0800

    开始进行简单测试

--- jest.config.ts
+++ jest.config.ts
@@ -66,6 +66,11 @@ export default {
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
+  // globals: {
+  //   'ts-jest': {
+  //     isolatedModules: true
+  //   }
+  // },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -152,6 +157,7 @@ export default {
   // The test environment that will be used for testing
   // testEnvironment: "jest-environment-node",
   testEnvironment: 'jsdom',
+  // testEnvironment: 'jest-environment-jsdom-global',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,21 @@
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-location-mock';
+
+// Object.defineProperty(global, 'localStorage', {
+//   value: {
+//     store: {} as Record<string, string>,
+//     setItem(key: string, value: string) {
+//       this.store[key] = value;
+//     },
+//     getItem(key: string) {
+//       return this.store[key];
+//     },
+//     removeItem(key: string) {
+//       delete this.store[key];
+//     },
+//     clear() {
+//       this.store = {};
+//     }
+//   },
+//   configurable: true
+// });
